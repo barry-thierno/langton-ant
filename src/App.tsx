31720ae351ab669c43559e4e8484617642819512
thiesbarrye <thsaidbarry@gmail.com>
2018-11-11 11:58:32 +0100

    init typescript babel

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-// import { CircularProgress } from "material-ui";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import * as React from "react";
+import React, { Component } from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import Grid from "./grid/Grid.container";
-import { AppBar } from "material-ui";
 
-export default () => (
-  <MuiThemeProvider>
-    <AppBar title="Langton Ant" />
-    <div className="center">
-      {/* <CircularProgress size={180} thickness={5} /> */}
-      <Grid linesNumber={21} rowsNumber={21} />
-    </div>
-  </MuiThemeProvider>
-);
+export interface ITest {
+  name: string;
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
